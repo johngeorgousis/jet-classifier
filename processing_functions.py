0dee30dd7457d6c23e1aef1b7327501137de7249
Ioannis Georgousis <johngeorgousis@outlook.com>
2020-11-18 15:20:12 +0000

    feat: edit function description and work on rotating image

--- processing_functions.py
+++ processing_functions.py
@@ -5,24 +5,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import time
 from IPython.display import display
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def preprocess(event1):
     '''
-    -Extracts no. of constituents
-    -Drops constituents column
+    Input: Series (event) to be processed
+    Output: Preprocessed Series
+    
+    -Drops constituents element
     -Replaces NaN values with 0
     -Converts all values to floats
-    
-    Input: Series (event) to be processed
-    Output: Processed Series, constituents Series 
     '''
     
     # Create series copy
     event = event1.copy(deep=True)
     
-    # Drop constituents from series
+    # Drop constituents 
     event = event.drop(event.index[0])
     
     # Replace NaN with 0
@@ -37,88 +46,92 @@ def preprocess(event1):
 # mydata_prepr = [preprocess(mydata.iloc[i]) for i in range(mydata.shape[0])]
 
 # mydata_prepr = pd.DataFrame(mydata_prepr)
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-def create_image(event1, R=1.5, pixels=60):
-    
-    '''
-    FIRST STEPS (BEFORE BINNING) NEED RETHINKING (based on whether expected input is list-like vector, Series, or DataFrame)
-    
-    Creates an image of single event, or multiple events (input can be either Series or DataFrame). If DataFrame, then average image is created.  
-    
-    Input: vector (event)
-    Output: array
-    
-    table: if df=True, then display the image as a DataFrame as well
-    '''
-    
-    # Turn into pd.Series
-    #event = pd.Series(event1)
-    event = pd.DataFrame(event1).T
-    
-    # If input is Series (single event) then turn into DataFrame. This makes it so that single events are processed correctly
-#     if isinstance(event, pd.Series):
-#         event = pd.DataFrame(event).T
-
-    # Initiate bin lists
-    bin_h = []
-    bin_f = []
-    bin_p = []
-
-    # Define max number of constituents 
-    max_const = event.shape[1] // 3
-
-    # For all rows
-    #for i in range(event.shape[0]):             
-
-    # For all constituents (I tested it using only meaningful constituents from first column and the code was slower)
-    for i in range(max_const):
-        # Add constituent's coordinates to bin lists
-        bin_h.append(list(event.iloc[0][::3])[i])
-        bin_f.append(list(event.iloc[0][1::3])[i])
-        bin_p.append(list(event.iloc[0][2::3])[i])
-
-# Tried not doing it for pT=0 constituents. Was less efficient
-#     i = 0
-#     while i < max_const and list(event.iloc[0][2::3])[i] != 0.:
-#         bin_h.append(list(event.iloc[0][::3])[i])
-#         bin_f.append(list(event.iloc[0][1::3])[i])
-#         bin_p.append(list(event.iloc[0][2::3])[i])
-#         i += 1
-
-    
-
-    # Turn lists into Series
-    bin_h = pd.Series(bin_h)
-    bin_f = pd.Series(bin_f)
-    bin_p = pd.Series(bin_p)
-
-   # Define no. of bins
-    bin_count = np.linspace(-R, R, pixels + 1)
-
-    # Create bins from -R to R (using bins vector)
-    bins = np.histogram2d(bin_h, bin_f, bins=bin_count, weights=bin_p)[0] # x and y are switch because when the bins were turned into a Series the shape[0] and shape[1] were switched
-
-    # Convert to DataFrame
-    image = bins
-    
-    return image
-
-
-
-
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
 
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+from scipy import ndimage
+
+def rotate(event, f_id_2):
+    
+    
+    while abs(event.iloc[f_id_2]) > 0.1:
+        ndimage.rotate(event, 1)
+        
+    return event
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def test(event, pixels=60, R=1.5, display=False):
+    
+    '''
+    Given an event from my_data return an image (this is for quick testing of transformation steps)
+    '''
+
+    image = np.zeros((pixels, pixels))
+    
+    event = pd.Series(event)
+    event = preprocess(event)
+    max123, f_id_2 = extract_max123(event)
+    event = center(event, max123)
+    
+    event = create_image(event, pixels=pixels, R=R)
+    event = pd.DataFrame(event)
+    event = rotate(event, f_id_2)
+    event = nd.array(event)
+    image += event
+   
+    
+    if display:
+        sns.heatmap(image)
+        return None
+    
+    return image    
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def average_image(pixels=60, R=1.5, event_no=12178, display=False):
     '''
-    pixels: Image Resolution, int.
-    event_no: # of events for which images be created. If int, then single image (faster) If list, then multiple images (slower)
+    Reads events directly from a file and creates an average image of the events. 
+    
+    pixels: int. Image Resolution
+    R: float. Fat jet radius
+    event_no: int/list. Number of events for which images are created. If int, then single image (faster). If list, then multiple images (slower)
+    display: boolean. Indicates whether images should be displayed automatically (return null) or returned as an ndarray. 
     '''
 
     image = np.zeros((pixels, pixels))                           # Define initial image
@@ -210,20 +223,102 @@ def average_image(pixels=60, R=1.5, event_no=12178, display=False):
                     images.append(image)
                     if a >= max(event_no):                        # Break if max sample size for average image is exceeded
                         return images
-                    
-                    
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def create_image(event1, R=1.5, pixels=60):
+    
+    '''    
+    Creates an image of single event.
+    
+    Input: Series (event)
+    Output: ndarray (image)
+    '''
+    
+    # Turn into pd.Series
+    #event = pd.Series(event1)
+    event = pd.DataFrame(event1).T
+    
+    # If input is Series (single event) then turn into DataFrame. This makes it so that single events are processed correctly
+#     if isinstance(event, pd.Series):
+#         event = pd.DataFrame(event).T
+
+    # Initiate bin lists
+    bin_h = []
+    bin_f = []
+    bin_p = []
+
+    # Define max number of constituents 
+    max_const = event.shape[1] // 3
+
+    # For all rows
+    #for i in range(event.shape[0]):             
+
+    # For all constituents (I tested it using only meaningful constituents from first column and the code was slower)
+    for i in range(max_const):
+        # Add constituent's coordinates to bin lists
+        bin_h.append(list(event.iloc[0][::3])[i])
+        bin_f.append(list(event.iloc[0][1::3])[i])
+        bin_p.append(list(event.iloc[0][2::3])[i])
+
+# Tried not doing it for pT=0 constituents. Was less efficient
+#     i = 0
+#     while i < max_const and list(event.iloc[0][2::3])[i] != 0.:
+#         bin_h.append(list(event.iloc[0][::3])[i])
+#         bin_f.append(list(event.iloc[0][1::3])[i])
+#         bin_p.append(list(event.iloc[0][2::3])[i])
+#         i += 1
+
+    
+
+    # Turn lists into Series
+    bin_h = pd.Series(bin_h)
+    bin_f = pd.Series(bin_f)
+    bin_p = pd.Series(bin_p)
+
+   # Define no. of bins
+    bin_count = np.linspace(-R, R, pixels + 1)
+
+    # Create bins from -R to R (using bins vector)
+    bins = np.histogram2d(bin_h, bin_f, bins=bin_count, weights=bin_p)[0] # x and y are switch because when the bins were turned into a Series the shape[0] and shape[1] were switched
+
+    # Convert to DataFrame
+    image = bins
+    
+    return image
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def extract_max123(event1):
 
     '''
-    Input: event (row). 
-    e.g. mydata_prep.iloc[0]
+    Takes an event and outputs a tuple containing 3 Series, each for the highest pT and its φ, η.
+    
+    Input: Series (event). 
 
-    Output[0]: [Series of 1st max p, φ, η]
-    Output[1]: [Series of 2nd max p, φ, η]
-    Output[2]: [Series of 3rd max p, φ, η]
+    Output[0]: [Series of 1st max pT, φ, η]
+    Output[1]: [Series of 2nd max pT, φ, η]
+    Output[2]: [Series of 3rd max pT, φ, η]
     '''
 
 
@@ -237,7 +332,7 @@ def extract_max123(event1):
 
 
 
-    # 1. Extract index of maximum pT
+    # 1. Extract index of 1st maximum pT
     maxid1 = pdata1.idxmax()
     maxlist1 = []
 
@@ -257,8 +352,10 @@ def extract_max123(event1):
     pdata2 = pdata1.copy(deep=True)
     pdata2.loc[maxid1] = 0
 
-    # 1. Extract index of maximum pT
+    # 1. Extract index of 2nd maximum pT
     maxid2 = pdata2.idxmax()
+    f_id_2 = maxid2 - 1
+    h_id_2 = maxid2 - 2
     maxlist2 = []
 
     # 2. Extract max η, φ, pT for event
@@ -269,7 +366,7 @@ def extract_max123(event1):
 
     # 3. Create & Display dataframe of max pT, η, φ
     row_max2 = pd.Series(data=maxlist2[0], index=['pT', 'φ', 'η'])
-
+    
 
 
 
@@ -277,7 +374,7 @@ def extract_max123(event1):
     pdata3 = pdata2.copy(deep=True)
     pdata3.loc[maxid2] = 0
 
-    # 1. Extract index of maximum pT
+    # 1. Extract index of 3rd maximum pT
     maxid3 = pdata3.idxmax()
     maxlist3 = []
 
@@ -291,25 +388,29 @@ def extract_max123(event1):
     row_max3 = pd.Series(data=maxlist3[0], index=['pT', 'φ', 'η'])
 
 
-
-    return row_max1, row_max2, row_max3
-
-
-
-
+    maxtuple = row_max1, row_max2, row_max3
+    
+    return maxtuple, f_id_2
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
 
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 def center(event1, max123, output='event', R=1.5, pixels=60):
     
     '''
     Centers image around (φ', η') = (0, 0). Both transformations are linear (so far). 
     
-    event1: the event (row) to be transformed
-    max123: list of 3 dataframes of max pT, η, φ. Obtained using the extract_max123() function
+    event1: Series (event)
+    max123: Tuple of 3 series of max pT, η, φ. Returned by extract_max123() function
     output: 'event' to return a Series of the transformed event1. 'image' to return a transformed dataframe representing an image 
     '''
     
@@ -377,13 +478,51 @@ def center(event1, max123, output='event', R=1.5, pixels=60):
         image = bins
         
         return image
-    
-    
-    
-    
+ #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
 
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
